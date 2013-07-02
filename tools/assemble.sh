f073f6d4915a64a126a700b7e38f391f7a8bf1ce
Dániel Bali <balijanosdaniel@gmail.com>
2013-07-02 14:56:43 +0200

    Added more parameters to the assembler script

--- tools/assemble.sh
+++ tools/assemble.sh
@@ -2,6 +2,6 @@
 
 rm -f _tmp*
 
-run/assembler $1 _tmp1.bin
+run/assembler $3 _tmp1.bin
 python tools/dummy_elf_patcher/patch_dummy.py tools/dummy_elf_patcher/sample_dummy.bin _tmp1.bin _tmp2.bin
-run/binary_gen 1 0 _tmp2.bin -
+run/binary_gen $1 $2 _tmp2.bin -
